dd2d788c1461c7db11d9665e44c09c6adf9346c6
Theodoros Sphikas <TSphikas@gmail.com>
2022-07-01 08:50:04 +0100

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+import Home from "./screens/Home.js";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+      style={{ alignItems: "center" }}
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Jotter"
+    >
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>blah</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
